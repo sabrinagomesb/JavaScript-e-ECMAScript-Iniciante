5726eca1f00d4e3bd8553406a1f582ac8b2e3a0f
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2022-11-18 12:02:39 -0300

    adiciona atualizacoes aula 11 e 12

---
+++ aula12/exerc11.js
@@ -0,0 +1,34 @@
+var idade = 17
+if (idade < 16) {
+  console.log('Não Vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
+
+/* var idade = 10
+if (idade < 16) {
+  console.log('Não vota')
+} else {
+    if (idade >= 16 && idade < 18) {
+      console.log('Voto opcional')
+    }
+}
+ */
+
+/* var idade = 16
+if (idade < 18) {
+  console.log('Não vota')
+} else {
+  console.log('Vota')
+} */
+
+
+/* var idade = 12
+if (idade < 18) {
+  console.log('Menor de idade')
+} else {
+    console.log('Maior de idade')
+} */
+
