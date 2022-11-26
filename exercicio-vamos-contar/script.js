f0a3481a734226c2fb791b11c9cae9cc3f97ba01
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2022-11-25 21:14:01 -0300

    altera exercicio aula 14

--- exercicio-vamos-contar/script.js
+++ exercicio-vamos-contar/script.js
@@ -9,16 +9,27 @@ function contar() {
   function iniciaContador() {
     resultado.innerHTML = "<strong>Contando...</strong><br>"
     // resultado = <strong>Contando...</strong>
-    
+
     let inicioNumero = Number(inicioElemento.value)
     let fimNumero = Number(fimElemento.value)
     let passoNumero = Number(passoElemento.value)
-    
-    for (let num = inicioNumero; num <= fimNumero; num += passoNumero) {
-      resultado.innerHTML += ` &#128073 ${num}`
-      // resultado = <strong>Contando...</strong> + emoji e nu
-      // resultado = <strong>Contando...</strong> + emoji e nu + emoji e nu + emoji e nu + emoji e nu
+
+    if (inicioNumero < fimNumero) {
+      for (let num = inicioNumero; num <= fimNumero; num += passoNumero) {
+        resultado.innerHTML += ` &#128073 ${num}`
+        // resultado = <strong>Contando...</strong> + emoji e nu
+        // resultado = <strong>Contando...</strong> + emoji e nu + emoji e nu + emoji e nu + emoji e nu
+      }
+    } else if (inicioNumero > fimNumero) {
+      for (let num = inicioNumero; num >= fimNumero; num -= passoNumero) {
+        resultado.innerHTML += ` &#128073 ${num}`
+        // resultado = <strong>Contando...</strong> + emoji e nu
+        // resultado = <strong>Contando...</strong> + emoji e nu + emoji e nu + emoji e nu + emoji e nu
+      }
+    } else if (inicioNumero == fimNumero) {
+      resultado.innerHTML += "Inicio igual ao fim, então não precisamos contar"
     }
+    
     resultado.innerHTML += "<br> &#128721 FIM."
   }
 
@@ -32,7 +43,7 @@ function contar() {
     resultado.innerHTML =
       "<strong>Impossível contar! Valor de FIM não pode ser 0.</strong>"
     // resultado = "<strong>Impossível contar! Valor de FIM não pode ser 0.</strong>"
-  } else if (Number(passoElemento.value) == 0) {
+  } else if (Number(passoElemento.value) <= 0) {
     alert("Passo inválido! Considerando PASSO 01")
     passoElemento.value = 1
     iniciaContador()
