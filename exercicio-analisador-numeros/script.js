99adabe868e96a1b023d3e1122b41bdd461e6d32
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2022-11-30 14:53:34 -0300

    refatora exercicio

--- exercicio-analisador-numeros/script.js
+++ exercicio-analisador-numeros/script.js
@@ -3,15 +3,34 @@ let listaElemento = document.getElementById("lista")
 let result = document.getElementById("result")
 let valores = []
 
-function adicionar() {
-  if (numeroElemento.value == 0 || numeroElemento.value > 100) {
-    alert("[ERRO] Digite um número entre 1 e 100.")
+function numeroValido(numero) {
+  if (Number(numero) >= 1 && Number(numero) <= 100) {
+    return true
   } else {
+    return false
+  }
+}
+
+function numeroRepetido(numero, lista) {
+  if (lista.indexOf(Number(numero)) != -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function adicionar() {
+  if (
+    numeroValido(numeroElemento.value) &&
+    !numeroRepetido(numeroElemento.value, valores)
+  ) {
     valores.push(Number(numeroElemento.value))
     let item = document.createElement("option")
     item.text = `Valor ${numeroElemento.value} adicionado.`
     listaElemento.appendChild(item)
     result.innerHTML = ""
+  } else {
+    alert("[ERRO] Digite um número único entre 1 e 100.")
   }
   numeroElemento.value = ""
   numeroElemento.focus()
@@ -26,11 +45,15 @@ function finalizar() {
     let menorNumber = valores[0]
     let somaNumber = 0
     let mediaNumber = 0
-    for (let pos in valores) {
-      somaNumber += valores[pos]
-      if (valores[pos] > maiorNumber) maiorNumber = valores[pos]
-      if (valores[pos] < menorNumber) menorNumber = valores[pos]
+
+    for (let posicaoAtual in valores) {
+      somaNumber += valores[posicaoAtual]
+      if (valores[posicaoAtual] > maiorNumber)
+        maiorNumber = valores[posicaoAtual]
+      if (valores[posicaoAtual] < menorNumber)
+        menorNumber = valores[posicaoAtual]
     }
+
     mediaNumber = somaNumber / quantNumber
 
     result.innerHTML = ""
@@ -39,8 +62,5 @@ function finalizar() {
     result.innerHTML += `<p>O menor valor informado foi ${menorNumber}.</p>`
     result.innerHTML += `<p>A soma dos valores inseridos é: ${somaNumber}.</p>`
     result.innerHTML += `<p>A média dos valores inseridos é: ${mediaNumber}.</p>`
-    
-    
-    
   }
 }
